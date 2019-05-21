49d5dd85a588d1586ce0749ca9cb697f0661417f
Lanranyan <44810787+Lanranyan@users.noreply.github.com>
2019-05-21 08:24:02 -0700

    Add files via upload

--- Tiled1/settings.py
+++ Tiled1/settings.py
@@ -44,7 +44,7 @@ BULLET_DAMAGE = 10
 
 # Mob settings
 MOB_IMG = 'zombie1_hold.png'
-MOB_SPEEDS = [150, 100, 75, 125]
+MOB_SPEEDS = [150, 100, 75, 125, 200, 300, 325]
 MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
 MOB_HEALTH = 100
 MOB_DAMAGE = 10
